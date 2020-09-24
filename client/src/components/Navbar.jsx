ed3070a01f89c2d9631225cf58a21b5b3f0ea2c6
Lucie Wang <lucie.wangxy@gmail.com>
2020-09-24 09:54:05 -0700

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import fire from '../img/Flat-Nintendo-Icons/PNG/Fire-Flower.png';
+import ice from '../img/Flat-Nintendo-Icons/PNG/Ice-Flower.png';
 
-const NavBar = () =>{
+const NavBar = (props) => {
 
-    return(
-        <div>
+    return (
+        <div className="top-nav">
             <h1>Welcome!</h1>
+            <div className='button'>
+                    <button onClick={props.startGame}>Start Game</button></div>
+            <div className='sideRight'>
+            <h2><img src={fire} width="20px" />  Score: {props.score}</h2>
+                    <h2><img src={ice} width="20px" />  Time: {props.time}</h2>
+            </div>
         </div>
     );
 }
